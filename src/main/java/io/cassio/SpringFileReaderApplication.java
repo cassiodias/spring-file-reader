2dde9cb8adf4d986fa26dcee2fa50257ce7253c5
Pivotal <pivotal@iMac-7.local>
2016-11-11 11:56:31 +0000

    adding bootstrap project

---
+++ src/main/java/io/cassio/SpringFileReaderApplication.java
@@ -0,0 +1,12 @@
+package io.cassio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFileReaderApplication.class, args);
+	}
+}
